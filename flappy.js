f5943e445a4f993d069ca1944dda69d5cf55f4a0
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-12-06 21:43:42 +0800

    created update and placepipes functions

--- flappy.js
+++ flappy.js
@@ -16,6 +16,15 @@ let bird = {
     height: birdHeight,
 }
 
+let pipeArray = []
+let pipeWidth = 54
+let pipeHeight = 512
+let pipeX = boardWidth
+let pipeY = 0
+
+let topPipeImg
+let bottomPipeImg
+
 
 window.onload = () => {
     board = document.getElementById('board');
@@ -29,4 +38,34 @@ window.onload = () => {
     birdImg.onload = () => {
         context.drawImage(birdImg, bird.x, birdY, birdWidth, birdHeight);
     }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "./img/toppipe.png"
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./img/bottompipe.png"
+
+    requestAnimationFrame(update)
+}
+
+function update(){
+    requestAnimationFrame(update)
+
+    context.clearRect(0, 0, board.width, board.height);
+    context.drawImage(birdImg, bird.x, birdY, birdWidth, birdHeight)
+}
+
+function placePipes(){
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2)
+
+    let topPipe = {
+        img: topPipeImg,
+        y: randomPipeY,
+        x: pipeX,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+
+    pipeArray.push(topPipe)
 }
